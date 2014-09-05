a97cd446f170f51cf0cae99d5ba79ffa3362e563
Xander Smeets <xander.smeets@outlook.com>
2014-09-05 19:45:41 +0200

    Update test-login.js
    Changed to an HTML page.

--- test-login.js
+++ test-login.js
@@ -1,2 +1,4 @@
+<script>
 var name = prompt("Please enter your name", "Demo");
-console.log("Welcome " + name + ", to the Discant network. You've been successfully logged in.")
+alert("Welcome " + name + ", to the Discant network. You've been successfully logged in.")
+</script>
